576d68a647cdae2ce614806594cc344eeac16953
Robertofcg <pscispscis@gmail.com>
2024-05-22 16:46:21 -0600

    cambio de diseño y funcionalidad

--- src/pages/productos copy.js
+++ src/pages/productos copy.js
@@ -104,35 +104,54 @@ function ProductList() {
                         </div>
                     </div>
                     <div className="col-md-10" style={{ height: '77vh', overflowY: 'scroll' }}>
-                        <div className="row">
-                            {datosFiltrados.map((producto, index) => (
-                                <div className="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4" key={index}>
-                                    <figure className="card card-product-grid">
-                                        <div className="img-wrap rounded bg-light">
-                                            <span className="topbar"></span>
-                                            <NavLink to={`/vistaProductosJava/${producto.id}`} activeclassname="active">
-                                                <img className="mix-blend-mode absolute transform hover:scale-105 transition duration-700 ease-out" src={`data:image/png;base64, ${producto.imagenes}`} width="320" height="180" alt={producto.nombre} />
-                                            </NavLink>
-                                        </div>
-                                        <figcaption className="card-product-info m-3">
-                                            <NavLink to={`/vistaProductosJava/${producto.id}`} activeclassname="active" className='title'>
-                                                {producto.nombre}
-                                            </NavLink>
-                                            <div className="price-wrap">
-                                                <span className="price">${producto.precio}</span>
+                        {datosFiltrados.length === 0 ? (
+                            <div className="flex items-center justify-center mt-20">
+                                <svg className="animate-spin h-10 w-10 mr-3 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.001 8.001 0 014 12H0c0 6.627 5.373 12 12 12v-4a7.946 7.946 0 01-6-2.709z"></path>
+                                </svg>
+                                <span className='font-semibold text-3xl'>Cargando...</span>
+                            </div>
+
+
+                        ) : (
+                            <div className="row">
+                                {datosFiltrados.map((producto, index) => (
+                                    <div className="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4" key={index}>
+                                        <figure className="card card-product-grid">
+                                            <div className="img-wrap rounded bg-light">
+                                                <span className="topbar"></span>
+                                                <NavLink to={`/vistaProductosJava/${producto.id}`} activeclassname="active">
+                                                    <img className="mix-blend-mode absolute transform hover:scale-105 transition duration-700 ease-out"
+                                                        src={`data:image/png;base64, ${producto.imagenes}`}
+                                                        width="320"
+                                                        height="180"
+                                                        alt={producto.nombre}
+                                                    />
+                                                </NavLink>
+
                                             </div>
-                                        </figcaption>
-                                        <footer className="card-footer">
-                                            <button className="btn btn-lg btn-outline-dark w-100 mt-2 agregarCarrito" onClick={() => agregarProductoCarrito(producto)}>Agregar a carrito</button>
-                                        </footer>
-                                    </figure>
-                                </div>
-                            ))}
-                        </div>
+                                            <figcaption className="card-product-info m-3">
+                                                <NavLink to={`/vistaProductosJava/${producto.id}`} activeclassname="active" className='title'>
+                                                    {producto.nombre}
+                                                </NavLink>
+                                                <div className="price-wrap">
+                                                    <span className="price">${producto.precio}</span>
+                                                </div>
+                                            </figcaption>
+                                            <footer className="card-footer">
+                                                <button className="btn btn-lg btn-outline-dark w-100 mt-2 agregarCarrito" onClick={() => agregarProductoCarrito(producto)}>Agregar a carrito</button>
+                                            </footer>
+                                        </figure>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
         </div>
+
 
 
     );
